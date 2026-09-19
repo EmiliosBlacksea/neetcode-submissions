bd1a9e72dbd7ac2ff0c4b4fcdb9a4b7d4e729afe
AIMIILIOS MAVROTHALASSITIS <139469340+EmiliosBlacksea@users.noreply.github.com>
2026-09-19 18:59:34 +0300

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.py
@@ -0,0 +1,33 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        counts = [0] * 26
+        most_f = 0
+        longest_length = 0
+        left = 0
+        right = 0
+        counts[ord(s[right]) - ord('A')] += 1
+        while right < len(s):
+            
+            most_f = max(counts)
+            gap = right - left  + 1
+            replace = gap - most_f
+            if replace <= k:
+                right += 1
+                if right < len(s):
+                    counts[ord(s[right]) - ord('A')] += 1
+                    most_f = max(most_f, counts[ord(s[right]) - ord('A')])
+                longest_length = max(longest_length, gap)
+            else:
+                if right == len(s) - 1 and longest_length >= gap:
+                    return longest_length
+                counts[ord(s[left]) - ord('A')] = max(0, counts[ord(s[left]) - ord('A')] - 1)
+                left += 1
+        return longest_length
+
+
+        
